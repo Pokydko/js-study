0b43ba4201d48d8dc726990d47328faac1d8715b
Anton Pokydko <AntonPokydko@gmail.com>
2024-04-21 03:32:41 +0300

    js m3

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,11 @@
-"use strict";
+'use strict';
 
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
-  return totalPrice > customerCredits
-    ? `Insufficient funds!`
-    : `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function slugify(title) {
+  return title.toLowerCase().split(' ').join('-');
 }
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+console.log(slugify('Arrays for begginers')); // "arrays-for-begginers" <br>
+console.log(slugify('English for developer')); // "english-for-developer" <br>
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript" <br>
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); //
+('how-to-become-a-junior-developer-in-two-weeks');
--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
-function formatMessage(message, maxLength) {
-  return message.length <= maxLength
-    ? message
-    : message.slice(0, maxLength) + `...`;
+function makeArray(firstArray, secondArray, maxLength) {
+  const arr = firstArray.concat(secondArray);
+  return arr.length > maxLength ? arr.slice(0, maxLength) : arr;
 }
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
